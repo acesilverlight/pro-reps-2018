b722028f1738c76751e30207de889b65275b26ec
Ace SilverLight <acesilverlight@outlook.com>
2018-10-08 10:59:54 +0530

    Initial Commit

---
+++ defend.sh
@@ -0,0 +1,23 @@
+mkdir milestone
+
+pwd
+
+cd /boot
+
+/boot
+
+cd
+
+cd milestone
+
+mkdir faster
+
+echo no days off
+
+#McDonalds, I am hungry
+
+cd /
+
+cd usr
+
+bash rep.sh
---
+++ rep.sh
@@ -0,0 +1,17 @@
+pwd
+
+cd
+
+man touch
+
+man ls
+
+mkdir fast
+
+mkdir furious
+
+cd fast
+
+touch fst.txt
+
+/home/acesilverlight/fast
